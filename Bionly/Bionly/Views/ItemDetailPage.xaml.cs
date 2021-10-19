1fa22b5a32a4cb8e1467affd63f1e39996d89c25
Tom <tom.mohr@t-app-germany.com>
2021-10-19 16:33:30 +0200

    Projektdateien hinzufügen.

---
+++ Bionly/Bionly/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Bionly.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Bionly.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
