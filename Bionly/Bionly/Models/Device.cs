9a8dd8ecd5d0d910376630e2160c0bbfbe1669c7
Tom <49612544+tomo2403@users.noreply.github.com>
2021-11-05 18:30:09 +0100

    Added edit and delete function to devices. SettingsPage temporary as default page

--- Bionly/Bionly/Models/Device.cs
+++ Bionly/Bionly/Models/Device.cs
@@ -1,14 +1,157 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace Bionly.Models
 {
-    public class Device
+    public class Device : INotifyPropertyChanged
     {
-        public string Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public static string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create);
 
+        private string _id;
+        public string Id
+        {
+            get => _id;
+            set
+            {
+                if (_id != value)
+                {
+                    _id = value;
+                    OnPropertyChanged(nameof(Id));
+                }
+            }
+        }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+        private string _description;
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged(nameof(Description));
+                }
+            }
+        }
+
+        private string _ipAdress;
+        public string IpAddress
+        {
+            get => _ipAdress;
+            set
+            {
+                if (_ipAdress != value && CheckAdress(value))
+                {
+                    _ipAdress = value;
+                    OnPropertyChanged(nameof(IpAddress));
+                }
+            }
+        }
+
+        public Device()
+        {
+
+        }
+
+        public async Task<bool> Delete()
+        {
+            try
+            {
+                File.Delete(path + $"/Devices/{Id}.json");
+                return true;
+            }
+            catch (DirectoryNotFoundException)
+            {
+                return true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", "Die App besitzt nicht die benötigten Berechtigungen um dieses Gerät zu löschen.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", ex.Message, "OK");
+            }
+            return false;
+        }
+        public async Task<bool> Save()
+        {
+            try
+            {
+                _ = Directory.CreateDirectory(path + $"/Devices");
+                File.WriteAllText(path + $"/Devices/{Id}.json", JsonConvert.SerializeObject(this, Formatting.Indented));
+                return true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", "Die App besitzt nicht die benötigten Berechtigungen um dieses Gerät zu speichern.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", ex.Message, "OK");
+            }
+            return false;
+        }
+        public static async Task<Device> Load(string file)
+        {
+            try
+            {
+                _ = Directory.CreateDirectory(path + $"/Devices");
+                return JsonConvert.DeserializeObject<Device>(File.ReadAllText(file));
+            }
+            catch (UnauthorizedAccessException)
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", "Die App besitzt nicht die benötigten Berechtigungen um dieses Gerät zu laden.", "OK");
+            }
+            catch (FileNotFoundException)
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", "Dieses Gerät wurde nicht gefunden.", "OK");
+            }
+            catch (Exception ex)
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", ex.Message, "OK");
+            }
+            return new();
+        }
+
+        public bool CheckAdress(string input)
+        {
+            if (IPAddress.TryParse(input, out IPAddress address))
+            {
+                _ipAdress = address.ToString();
+                return true;
+            }
+            return false;
+        }
+        public IPAddress GetAdress()
+        {
+            return IPAddress.Parse(IpAddress);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void OnPropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
     }
 }
--- Bionly/Bionly/ViewModels/SettingsViewModel.cs
+++ Bionly/Bionly/ViewModels/SettingsViewModel.cs
@@ -1,14 +1,104 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Bionly.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
+        public ObservableCollection<Models.Device> Devices { get; set; } = new();
+        public static readonly string[] strings = new string[7];
+
+        public ICommand LoadAllDevices => new Command(async () =>
+        {
+            Devices.Clear();
+            foreach (string file in Directory.GetFiles(Models.Device.path + "\\Devices\\", "*.json", SearchOption.TopDirectoryOnly))
+            {
+                Models.Device d = await Models.Device.Load(file);
+                Devices.Add(d);
+            }
+        });
+
+        public ICommand AddNewDevice => new Command(async () =>
+        {
+            string result = await Application.Current.MainPage.DisplayPromptAsync("Neues Gerät", "Wie soll das Gerät heißen?", strings[1], strings[0], "Neues Gerät", keyboard: Keyboard.Text);
+            if (!string.IsNullOrWhiteSpace(result))
+            {
+                Models.Device d = new();
+                d.Name = result;
+
+                bool answer = await Application.Current.MainPage.DisplayAlert("Beschreibung hinzufügen", "Möchten Sie eine Beschreibung zum Gerät hinzufügen?", "Ja", "Nein");
+                if (answer)
+                {
+                    result = await Application.Current.MainPage.DisplayPromptAsync(d.Name, "Geben Sie eine Beschreibung ein", strings[1], strings[0], keyboard: Keyboard.Text);
+                    if (!string.IsNullOrWhiteSpace(result))
+                    {
+                        d.Description = result;
+                    }
+                }
+
+                bool valid;
+                bool cancel = true;
+                do
+                {
+                    result = await Application.Current.MainPage.DisplayPromptAsync(d.Name, "Geben Sie die IP-Adresse des Gerätes ein", strings[2], strings[0], "127.0.0.1", keyboard: Keyboard.Text);
+                    if (result == null)
+                    {
+                        valid = true;
+                    }
+                    else
+                    {
+                        valid = await SetIpAdress(d, result);
+                        if (valid)
+                        {
+                            cancel = false;
+                        }
+                    }
+                } while (!valid);
+
+                if (!cancel)
+                {
+                    byte[] codebytes = new byte[8];
+                    string code;
+                    do
+                    {
+                        using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+                        {
+                            rng.GetBytes(codebytes);
+                        }
+                        code = BitConverter.ToString(codebytes).ToLower().Replace("-", "");
+                    } while (File.Exists(Models.Device.path + $"/Devices/{code}.json"));
+                    d.Id = code;
+                    _ = await d.Save();
+                    Devices.Add(d);
+                }
+            }
+        });
+
         public SettingsViewModel()
         {
             Title = "Einstellungen";
+            strings[0] = "Abbrechen";
+            strings[1] = "Weiter";
+            strings[2] = "Speichern";
+            strings[3] = "Entfernen";
+            strings[4] = "Name";
+            strings[5] = "Beschreibung";
+            strings[6] = "IP-Adresse";
+        }
+
+        public static async Task<bool> SetIpAdress(Models.Device d, string input)
+        {
+            if (!d.CheckAdress(input))
+            {
+                await Application.Current.MainPage.DisplayAlert("Fehler", input + " ist keine gültige IP-Adresse!", "OK");
+                return false;
+            }
+            return true;
         }
     }
 }
