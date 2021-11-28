38b7a97f48fc897810d931f7bd524a4899c82c4b
Tom <49612544+tomo2403@users.noreply.github.com>
2021-11-28 14:46:47 +0100

    rtsp test

--- Bionly/Bionly/ViewModels/LiveviewViewModel.cs
+++ Bionly/Bionly/ViewModels/LiveviewViewModel.cs
@@ -1,14 +1,82 @@
-﻿using System;
+﻿using LibVLCSharp.Shared;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Bionly.ViewModels
 {
     public class LiveviewViewModel : BaseViewModel
     {
+        new public event PropertyChangedEventHandler PropertyChanged;
+
         public LiveviewViewModel()
         {
             Title = "Live Ansicht";
+            Initialize();
         }
+
+        private LibVLC LibVLC { get; set; }
+
+        private MediaPlayer _mediaPlayer;
+        public MediaPlayer MediaPlayer
+        {
+            get => _mediaPlayer;
+            private set => Set(nameof(MediaPlayer), ref _mediaPlayer, value);
+        }
+
+        private bool IsLoaded { get; set; }
+        private bool IsVideoViewInitialized { get; set; }
+
+        private void Set<T>(string propertyName, ref T field, T value)
+        {
+            if (field == null && value != null || field != null && !field.Equals(value))
+            {
+                field = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private void Initialize()
+        {
+            Core.Initialize();
+
+            LibVLC = new LibVLC();
+            var media = new Media(LibVLC, new Uri("rtsp://demo:demo@ipvmdemo.dyndns.org:5541/onvif-media/media.amp?profile=profile_1_h264&sessiontimeout=60&streamtype=unicast"));
+
+            MediaPlayer = new MediaPlayer(LibVLC)
+            {
+                Media = media,
+            };
+
+            media.Dispose();
+        }
+
+        public void OnAppearing()
+        {
+            IsLoaded = true;
+            Play();
+        }
+
+        internal void OnDisappearing()
+        {
+            //MediaPlayer.Dispose();
+            //LibVLC.Dispose();
+        }
+
+        public void OnVideoViewInitialized()
+        {
+            IsVideoViewInitialized = true;
+            Play();
+        }
+
+        private void Play()
+        {
+            if (IsLoaded && IsVideoViewInitialized)
+            {
+                MediaPlayer.Play();
+            }
+        }
+
     }
 }
--- Bionly/Bionly/Views/LiveviewPage.xaml.cs
+++ Bionly/Bionly/Views/LiveviewPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Bionly.ViewModels;
+using LibVLCSharp.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,5 +18,23 @@ namespace Bionly.Views
         {
             InitializeComponent();
         }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            ((LiveviewViewModel)BindingContext).OnAppearing();
+        }
+
+        protected override void OnDisappearing()
+        {
+            base.OnDisappearing();
+            ((LiveviewViewModel)BindingContext).OnDisappearing();
+        }
+
+        private void VideoView_MediaPlayerChanged(object sender, MediaPlayerChangedEventArgs e)
+        {
+            ((LiveviewViewModel)BindingContext).OnVideoViewInitialized();
+        }
+
     }
 }
